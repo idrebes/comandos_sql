7d37ad8c3c60bdb36b173a1bc11d7e0f2a41570c
Iago Drebes <81392507+idrebes@users.noreply.github.com>
2021-09-08 22:12:55 -0300

    Update sql_educacao.sql

--- sql_educacao.sql
+++ sql_educacao.sql
@@ -87,3 +87,22 @@ n.nome, n.idade_inicio, n.idade_fim
 FROM candidatos c, niveis_ensino n
 WHERE DATE_PART('year', age(data_nascimento)) >= idade_inicio 
 AND DATE_PART('year', age(data_nascimento)) < idade_fim;
+
+--LISTAR A TURMA DA ESCOLA DE ACORDO COM A FAIXA ETÁRIA DO CANDIDATO
+SELECT c.nome, c.data_nascimento, t.id_turma as turma, n.idade_inicio, n.idade_fim
+FROM candidatos_escolas ce, candidatos c, niveis_ensino n, turmas t
+WHERE DATE_PART('year', age(data_nascimento)) >= idade_inicio 
+AND DATE_PART('year', age(data_nascimento)) < idade_fim
+AND c.id = ce.id_candidato AND n.id = t.id_nivel_ensino 
+AND t.id_escola = ce.id_escola
+ORDER BY c.id
+
+--ATUALIZAR A TURMA DENTRO DE CANDIDATOS_ESCOLAS
+UPDATE candidatos_escolas ce1
+SET id_turma = (SELECT t.id
+FROM candidatos_escolas ce, candidatos c, niveis_ensino n, turmas t
+WHERE DATE_PART('year', age(data_nascimento)) >= idade_inicio 
+AND DATE_PART('year', age(data_nascimento)) < idade_fim
+AND c.id = ce.id_candidato AND n.id = t.id_nivel_ensino 
+AND t.id_escola = ce.id_escola
+AND ce1.id_candidato = c.id)
