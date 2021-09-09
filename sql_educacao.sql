6f5a67493d0a2d173bd6554a9a458e75332d9c60
Iago Drebes <81392507+idrebes@users.noreply.github.com>
2021-09-08 21:31:03 -0300

    Update sql_educacao.sql

--- sql_educacao.sql
+++ sql_educacao.sql
@@ -96,12 +96,15 @@ SELECT * FROM turmas
 
 SELECT * FROM niveis_ensino
 
-SELECT * FROM candidatos c, 
-
-UPDATE candidatos_escolas
-SET id_turma.candidatos_escolas = data_nascimento.candidatos
-WHERE id_escola = 1;
-
-SELECT id, DATE_PART('year', age(data_nascimento)) as idade, data_nascimento
+--LISTAR idade DO CANDIDATO
+SELECT id, DATE_PART('year', age(data_nascimento)) as idade, 
+data_nascimento 
 FROM candidatos
-ORDER BY DATE_PART ('year', age(data_nascimento));
+ORDER BY DATE_PART('year', age(data_nascimento));
+
+--LISTAR O NIVEL DE ENSINO DO CANDIDATO
+SELECT c.id, c.data_nascimento, DATE_PART('year', age(data_nascimento)) as idade,
+n.nome, n.idade_inicio, n.idade_fim
+FROM candidatos c, niveis_ensino n
+WHERE DATE_PART('year', age(data_nascimento)) >= idade_inicio 
+AND DATE_PART('year', age(data_nascimento)) < idade_fim;
